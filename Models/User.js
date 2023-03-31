89b4459254f0d786ae770a72183ca6216b65049d
Prashant Abhishek Lugun <prashantlugun@gmail.com>
2023-03-31 19:50:02 +0530

    New Error Row by show

--- Models/User.js
+++ Models/User.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  first_name: { type: String, required: true, match: /^[a-zA-Z]+$/ },
+  // first_name: { type: String, required: true, match: /^[a-zA-Z]+$/ },
+  first_name: { type: String, required: true},
   last_name: { type: String, required: true },
-  company_name: { type: String, required: true },
-  address: { type: String, required: true },
-  city: { type: String, required: true },
-  province: { type: String, required: true },
-  postal: { type: String, required: true },
-  phone1: { type: String, required: true },
-  phone2: { type: String, required: true },
-  email: { type: String, required: true, match: /^\S+@\S+\.\S+$/ },
-  web: { type: String, required: true }
+  // company_name: { type: String, required: true },
+  // address: { type: String, required: true },
+  // city: { type: String, required: true },
+  // province: { type: String, required: true },
+  // postal: { type: String, required: true },
+  // phone1: { type: String, required: true },
+  // phone2: { type: String, required: true },
+  // email: { type: String, required: true, match: /^\S+@\S+\.\S+$/ },
+  email: { type: String, required: true},
+  // web: { type: String, required: true }
 }, { timestamps: true, }
 );
 
--- controllers/csvController.js
+++ controllers/csvController.js
@@ -1,58 +1,127 @@
 const fs = require('fs'), path = require('path'), csv = require('fast-csv'), User = require("../Models/User");
-// Validation middleware function
-function validateUser(user) {
-  const { first_name, last_name, company_name, address, city, province, postal, phone1, phone2, email, web } = user;
 
-
-  // Check that the name field only contains alphabets
-  if (!/^[a-zA-Z]+$/.test(first_name)) {
-    throw new Error('Invalid First Name');
-  }
-  if (!/^[a-zA-Z]+$/.test(last_name)) {
-    throw new Error('Invalid Last Name');
-  }
-
-  // Check that the email field is in the correct format
-  if (!/^\S+@\S+\.\S+$/.test(email)) {
-    throw new Error('Invalid email');
-  }
-}
 exports.create = async (req, res) => {
-  const file = path.join(__dirname, '../', '/public/csv/' + req.file.filename);
-  const totalRecords = [];
-  let error = false;
   try {
+    if (req.file == undefined) {
+      return res.status(400).send("Please upload a CSV file!");
+    }
+
+    let users = [];
+    let file = path.join(__dirname, '../', '/public/csv/' + req.file.filename);
+
+    // console.log("path", file);
+
+
+    let emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    let alphabeticPattern = /^[A-Za-z]+$/;
+    let successCount = 0;
+    let errorCount = 0;
+    let errorArray = [];
     fs.createReadStream(file)
       .pipe(csv.parse({ headers: true }))
-      .on('error', error => console.error(error))
-      .on('data', async (row) => {
-        try {
-          validateUser(row);
-          totalRecords.push(row);
-        } catch (err) {
-          error = {
-            status: true,
-            message: err.message
-          };
-        }
+      .validate(data => data.first_name !== '' && data.last_name !== '' && emailPattern.test(data.email) && alphabeticPattern.test(data.first_name))
+      .on("error", (error) => {
+        throw error.message;
       })
-      .on('end', async rowCount => {
-        // Delete the CSV file
-        fs.unlink(file, (err) => {
-          if (err) {
-            console.error(err);
-          }
-        });
+      .on("data", (row) => {
+        successCount++;
+        users.push(row);
+        console.log(`ROW=${JSON.stringify(row)}`)
+      })
+      .on('data-invalid', (row, rowNumber) => {
+        errorCount++;
+        errorArray.push(`Invalid [rowNumber=${rowNumber}] [row=${JSON.stringify(row)}]`)
+        console.log(`Invalid [rowNumber=${rowNumber}] [row=${JSON.stringify(row)}]`)
+      })
+      .on("end", (rowCount) => {
+        if (errorCount < 0) {
+          console.log(`Parsed ${rowCount} rows`)
+          User.insertMany(users, {
+            validate: true,
+          })
+          .then(async () => {
+              res.status(200).json({
+                error: false,
+                success:
+                  `Uploaded ${successCount} row successfully from ` + req.file.originalname,
+                failed: `Uploaded ${errorCount} row failed from ` + req.file.originalname,
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+              res.status(500).json({
+                error: error.message,
+                failed: `Fail to import ${users.length} row into database!`,
 
-        if (error.status) {
-          res.status(400).json({ err: error.message, status: 'Invalid CSV file' });
-        } else {
-          const users = await User.insertMany(totalRecords);
-          res.status(200).json({ status: 'CSV file uploaded', data: users });
+              });
+            });
+        }else{
+          res.status(500).json({
+            message: errorArray,
+            failed: `${errorCount} row failed ${req.file.originalname}. So It Can't be Uploaded`,
+          });
         }
       });
-
   } catch (error) {
-    res.status(400).json(error)
+    console.log(error);
+    console.log(error.message);
+    res.status(500).json({
+      error: error.message,
+      failed: "Could not upload the file: " + req.file.originalname,
+    });
   }
+
+
+
+
+
+
+
+
+  // try {
+  //   fs.createReadStream(file)
+  //     .pipe(csv.parse({ headers: true }))
+  //     .validate(data => console.log(data))
+  //     .on('error', error => {
+  //       throw error.message;
+  //     })
+  //     .on('data', async (row) => {
+  //       try {
+  //         successCount++;
+  //         totalRecords.push(row);
+  //         console.log(`ROW=${JSON.stringify(row)}`)
+  //       } catch (err) {
+  //         console.log(err.message);
+  //         error = {
+  //           status: true,
+  //           message: err.message
+  //         };
+  //       }
+  //     })
+  //     .on('data-invalid', (row, rowNumber, error) => {
+  //       errorCount++;
+  //       console.log(`${error.message}`);
+  //       console.log(`Invalid [rowNumber=${rowNumber}] [row=${JSON.stringify(row)}] : ${error}`)
+  //     })
+  //     .on('end', async rowCount => {
+  //       console.log(`Parsed ${rowCount} rows`)
+  //       // Delete the CSV file
+  //       fs.unlink(file, (err) => {
+  //         if (err) {
+  //           console.error(err);
+  //         }
+  //       });
+
+  //       if (error.status) {
+  //         res.status(400).json({ err: error.message, status: 'Invalid CSV file' });
+  //       } else {
+  //         // const users = await User.insertMany(totalRecords);
+  //         // res.status(200).json({ status: 'CSV file uploaded', data: users });
+  //         res.status(200).json({ status: 'CSV file uploaded' });
+  //       }
+  //     });
+
+  // } catch (error) {
+  //   res.status(400).json(error)
+  // }
 };
